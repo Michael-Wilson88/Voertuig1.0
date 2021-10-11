602461ded6d0fc654b0466a792df3a1010777251
Michael-Wilson88 <michaelwilson2211@gmail.com>
2021-10-11 21:17:18 +0200

    Adjusted controller

---
+++ src/main/java/com/example/Voertuig/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.Voertuig.controller;
+
+// De reservering die een User gemaakt heeft moet zichtbaar zijn. Dan wordt het zoiets: app/user/bookings
+// Alle reserveringen die er zijn moeten alleen voor de ADMIN zichtbaar zijn. app/bookings
+// Ik moet dit dus waarschijnlijk vanaf de controller definieren en niet vanuit de servicelayer, in de service layer moet de methode denk ik geen responseEntity zijn
+
+// Check of ik 2 services kan aanroepen in deze controller class
+public class UserController {
+
+    private
+}
+
+//Endpoints voor user zijn:
+///user/bookings
+///user/vehicles ?
